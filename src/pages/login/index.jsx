3b5646d74e2152b53feb047dc5eba4bf99869365
TruongTanNghia <tannghiaxp@gmail.com>
2023-11-28 20:34:21 +0700

    fix: fix size

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,15 +1,23 @@
 import Logo from "../../../public/images/Logo.png";
 import LogoFacebook from "../../../public/images/facebook-2-logo-png-transparent-1.png";
-
+import Line from "../../../public/images/line-14.svg";
 import LogoGoogle from "../../../public/images/google-g-logo-1.png";
 const Login = () => {
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center">
         <div className="w-[689px] h-[699px] bg-neutral-100 rounded-2xl border border-black border-opacity-25">
-          <div className="absolute text-black text-base font-semibold font-['COCOGOOSE']">
-            Register
+          <div className="absolute ml-[557px] mt-[19px] flex flex-row text-black text-base font-semibold font-COCOGOOSE">
+            <span className="w-[52px] inline-block border-b-2 border-sky-600 text-[16px]">
+              Register
+            </span>
+            <div className=" ml-[68px] mt-[6px] w-[18px] h-[18px] absolute">
+              <div className="w-[18px] h-[18px] mt-[3px] left-0 top-0 absolute">
+                <img src={Line} alt="Error" className="" />
+              </div>
+            </div>
           </div>
+
           <div className="pt-[27px] ">
             <div className="flex flex-col items-center">
               <img className="w-[151px] h-[126px] mb-[29px]" src={Logo} />
